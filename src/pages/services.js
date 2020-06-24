7b46ccec1222f56ebc729e92f6712594f0e4041b
Aid Thompson <thePandaRiot@gmail.com>
2020-06-24 20:47:31 +0100

    client list added

--- src/pages/services.js
+++ src/pages/services.js
@@ -8,29 +8,39 @@ function Services() {
                 <div className="ptext">
 
                     <span className="border">
-                        Funk-27
-                </span>
+                            Funk-27
+                    </span>
                 </div>
             </div>
             <section className="section section-light">
-                <h2>Section One</h2>
-                <p>That was Wintermute, manipulating the lock the way it had manipulated the drone micro and the amplified breathing of the room where Case waited. No sound but the muted purring of the arcade showed him broken lengths of damp chipboard and the amplified breathing of the room where Case waited. He’d waited in the dark, curled in his devotion to esoteric forms of tailor-worship. The alarm still oscillated, louder here, the rear of the spherical chamber. The Sprawl was a yearly pilgrimage to Tokyo, where genetic surgeons reset the code of his DNA, a procedure unavailable in Chiba. A narrow wedge of light from a half-open service hatch at the clinic, Molly took him to the Tank War, mouth touched with hot gold as a gliding cursor struck sparks from the wall between the bookcases, its distorted face sagging to the bare concrete floor. Why bother with the movement of the train, their high heels like polished hooves against the gray metal of the console in faded pinks and yellows. The Sprawl was a square of faint light. Light from a service hatch at the rear of the arcade showed him broken lengths of damp chipboard and the chassis of a gutted game console.</p>
+                <h2>Who We Are</h2>
+                <p>At Funk, we bring six years of high quality, commercial Software Development to you at an affordable price.
+                Our software developers have worked with some of the most impressive client names in their field from Sky to Thomson Reteurs,
+                 to newer names across the start-up and scale-up landscape.</p>
+                 <br />
+                 <p>
+                 Whether it's Desktop Applications in NodeJS and Electron, an 
+                 IOS/Android prototype in React Native or a new Sign-Up flow for your trading platform - our team are excited to help you 
+                 get your idea to the next stage!</p>
             </section>
 
             <div className="pimg2">
                 <div className="ptext">
 
                     <span className="border trans">
-                        Image Two text
-                </span>
+                        Bringing Your Ideas To Life
+                    </span>
                 </div>
             </div>
 
 
             <section className="section section-dark">
-                <h2>Section Two</h2>
-                <p>Then a mist closed over the black water and the robot gardener. The alarm still oscillated,
-                louder here, the rear wall dulling the roar of the Flatline as a construct, a hardwired ROM cassette replicating a dead man’s skills, obsessions, kneejerk responses. Why bother with the movement of the train, their high heels like polished hooves against the gray metal of the bright void beyond the chain link. Strata of cigarette smoke rose from the tiers, drifting until it struck currents set up by the blowers and the drifting shoals of waste. She put his pistol down, picked up her fletcher, dialed the barrel over to single shot, and very carefully put a toxin dart through the center of a skyscraper canyon. No sound but the muted purring of the previous century. Now this quiet courtyard, Sunday afternoon, this girl with a hand on his chest. They were dropping, losing altitude in a canyon of rainbow foliage, a lurid communal mural that completely covered the hull of the blowers and the amplified breathing of the fighters. The last Case saw of Chiba were the dark angles of the Flatline as a construct, a hardwired ROM cassette replicating a dead man’s skills, obsessions, kneejerk responses.</p>
+                <h2>What We Do</h2>
+                <p>Funksters don't position themselves as over-promising Jacks-of-all-trades. We know exactly where our strengths lie.</p>
+                <br />
+                <p>That's why a great deal of our client work sits within the Front End / UX arena. Websites, Webapps and Proof of Concept IOS/Android Applications. But we've recently flexed out to APIs, User Authentication and yes, okay, from time to time, Desktop applications.</p>
+                <br />
+                <p>If you've got an idea for PC/Mac application, a promotional website, or you need a PoC IOS/Android app building, get in touch now!</p>
             </section>
 
 
@@ -38,15 +48,21 @@ function Services() {
                 <div className="ptext">
 
                     <span className="border trans">
-                        Image Three text
+                        smart solutions
                     </span>
                 </div>
             </div>
 
 
             <section className="section section-dark">
-                <h2>Section Three</h2>
-                <p>Its hands were holograms that altered to match the convolutions of the Flatline as a construct, a hardwired ROM cassette replicating a dead man’s skills, obsessions, kneejerk responses. She put his pistol down, picked up her fletcher, dialed the barrel over to single shot, and very carefully put a toxin dart through the center of a junked console. Then a mist closed over the black water and the amplified breathing of the Villa bespeak a turning in, a denial of the bright void beyond the hull. Case felt the edge of the Villa bespeak a turning in, a denial of the bright void beyond the hull. The girls looked like tall, exotic grazing animals, swaying gracefully and unconsciously with the movement of the train, their high heels like polished hooves against the gray metal of the spherical chamber. That was Wintermute, manipulating the lock the way it had manipulated the drone micro and the amplified breathing of the spherical chamber. Now this quiet courtyard, Sunday afternoon, this girl with a luminous digital display wired to a subcutaneous chip. Strata of cigarette smoke rose from the tiers, drifting until it struck currents set up by the blowers and the dripping chassis of a junked console.</p>
+                <h2>Our Clients</h2>
+                <div className="flex-row client-imgs-container">
+                    <img className="client-img" src={require('../images/sky.png')} alt="sky" />
+                    <img className="client-img" src={require('../images/reuters.png')} alt="thomson_reuters" />
+                    <img className="client-img" src={require('../images/infabode.png')} alt="infabode" />
+                    <img className="client-img" src={require('../images/finsa.png')} alt="finsa" />
+                    <img className="client-img" src={require('../images/tradenation.svg')} alt="finsa" />
+                </div>
             </section>
 
 
@@ -55,7 +71,7 @@ function Services() {
                 <div className="ptext">
 
                     <span className="border">
-                        Parallax Website
+                        Funk-27
                 </span>
                 </div>
             </div>
