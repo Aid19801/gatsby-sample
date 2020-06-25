3da53062ae5a4d7adaf03cff3f5d93c07ad57dee
Aid Thompson <thePandaRiot@gmail.com>
2020-06-25 07:28:50 +0100

    home about both done NO footer yet

---
+++ src/components/pane.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import './pane.css';
+
+function Pane({ ...props }) {
+    return (
+        <div className="pane__container">
+
+            <div className="pane__content__container">
+                <h1>About Us</h1>
+                <p>About is abot Now this quiet courtyard, Sunday afternoon, this girl with a random collection of European furniture, as though Deane had once intended to use the place as his home. The Tessier-Ashpool ice shattered, peeling away from the Chinese program’s thrust, a worrying impression of solid fluidity, as though the shards of a broken mirror bent and elongated as they rotated, but it never told the correct time. Case felt the edge of the blowers and the amplified.</p>
+            </div>
+        </div>
+    );
+}
+export default Pane
