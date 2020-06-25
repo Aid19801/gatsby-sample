38984a5a784b460ca5ddc4c5aedaa4ce0f4bf14c
Aid Thompson <thePandaRiot@gmail.com>
2020-06-25 10:39:24 +0100

    home about both done WITH footer

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import withLayout from '../components/layout1';
+import RowOfBoxes from '../components/RowOfBoxes';
 import './page.css';
 
 function Services() {
@@ -9,39 +10,30 @@ function Services() {
                 <div className="ptext">
 
                     <span className="border">
-                            Funk-27
+                            Services
                     </span>
                 </div>
             </div>
-            <section className="section section-light">
-                <h2>Who We Are</h2>
-                <p>At Funk, we bring six years of high quality, commercial Software Development to you at an affordable price.
-                Our software developers have worked with some of the most impressive client names in their field from Sky to Thomson Reteurs,
-                 to newer names across the start-up and scale-up landscape.</p>
-                 <br />
-                 <p>
-                 Whether it's Desktop Applications in NodeJS and Electron, an 
-                 IOS/Android prototype in React Native or a new Sign-Up flow for your trading platform - our team are excited to help you 
-                 get your idea to the next stage!</p>
+            <section className="section section-dark">
+                <RowOfBoxes />
             </section>
 
             <div className="pimg2">
                 <div className="ptext">
 
                     <span className="border trans">
-                        Bringing Your Ideas To Life
+                        Get Funktional
                     </span>
                 </div>
             </div>
 
 
             <section className="section section-dark">
-                <h2>What We Do</h2>
-                <p>Funksters don't position themselves as over-promising Jacks-of-all-trades. We know exactly where our strengths lie.</p>
+                <h2>Get Funktional</h2>
+                <p>Whether you're a start-up looking for your first Proof Of Concept app or a business with hundreds of employees considering a new timesheeting system - we're here to look at the options with you and to deliver the technology you need.</p>
                 <br />
-                <p>That's why a great deal of our client work sits within the Front End / UX arena. Websites, Webapps and Proof of Concept IOS/Android Applications. But we've recently flexed out to APIs, User Authentication and yes, okay, from time to time, Desktop applications.</p>
+                <p>We span across the Front End and Middleware layers to bring you a slick user experience to attract the investment or customers you need to thrive.</p>
                 <br />
-                <p>If you've got an idea for PC/Mac application, a promotional website, or you need a PoC IOS/Android app building, get in touch now!</p>
             </section>
 
 
@@ -49,20 +41,29 @@ function Services() {
                 <div className="ptext">
 
                     <span className="border trans">
-                        smart solutions
+                        We Live Tech
                     </span>
                 </div>
             </div>
 
 
             <section className="section section-dark">
-                <h2>Our Clients</h2>
-                <div className="flex-row client-imgs-container">
-                    <img className="client-img" src={require('../images/sky.png')} alt="sky" />
-                    <img className="client-img" src={require('../images/reuters.png')} alt="thomson_reuters" />
-                    <img className="client-img" src={require('../images/infabode.png')} alt="infabode" />
-                    <img className="client-img" src={require('../images/finsa.png')} alt="finsa" />
-                    <img className="client-img" src={require('../images/tradenation.svg')} alt="finsa" />
+                <h2 className="orange">Our Developers Have Worked On These Exciting Projects</h2>
+                <div className="flex-col client-projects">
+                    <h2>Sky Go</h2>
+                    <p>Desktop Application</p>
+
+                    <h2>Infabode.com</h2>
+                    <p>Server Side Rendered Web-App</p>
+
+                    <h2>Tradenation</h2>
+                    <p>User-Authentication Flow</p>
+
+                    <h2>#ThatDadsApp</h2>
+                    <p>Proof Of Concept iOS/Android App</p>
+
+                    <h2>The Panda Riot</h2>
+                    <p>Desktop Application & Website</p>
                 </div>
             </section>
 
