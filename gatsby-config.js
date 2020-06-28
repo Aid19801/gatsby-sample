b9491c1271ead42d48b20e838b622667530c284b
Aid Thompson <thePandaRiot@gmail.com>
2020-06-28 09:48:36 +0100

    trying to fix seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
     // defaultDescription: `Welcome to the future! Funk-27 is a Front End Design and Software Delivery agency based in the South East of England.`,
     author: `@funkTwentySeven`,
     twitterUsername: `@funkTwentySeven`,
-    url: `https://www.funk27.com`,
-    image: `/assets/startup.jpg`,
+    url: `http://www.funk27.com`,
+    image: `/assets/startup.png`,
     titleTemplate: `F27`
   },
   plugins: [
