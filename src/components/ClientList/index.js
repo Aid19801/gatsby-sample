30b17d78dd448cdff0b02084d8d07b046d6853d4
Aid Thompson <thePandaRiot@gmail.com>
2020-07-02 06:22:26 +0100

    fade in for cients

--- src/components/ClientList/index.js
+++ src/components/ClientList/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { gsap, Power3 } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
 import '../../styles/ClientList.css';
 
 function ClientList({ ...props }) {
 
+    let divRef = useRef(null)
+    let h2ref = useRef(null)
+
+    useEffect(() => {
+        if (typeof window !== `undefined`) {
+            gsap.registerPlugin(ScrollTrigger)
+            gsap.core.globals("ScrollTrigger", ScrollTrigger)
+        }
+    }, [])
+
+    useEffect(() => {        
+        gsap.fromTo(divRef, {
+            opacity: 0,
+            y: 100,
+        }, {
+            scrollTrigger: ".client-imgs-container", // start the animation when ".box" enters the viewport (once)
+            ease: Power3.ease,
+            opacity: 1,
+            duration: .8,
+            y: 0,
+        });
+
+        gsap.fromTo(h2ref, {
+            opacity: 0,
+            y: 100,
+        }, {
+            scrollTrigger: ".clients-title", // start the animation when ".box" enters the viewport (once)
+            ease: Power3.ease,
+            opacity: 1,
+            duration: .8,
+            y: 0,
+        })
+    }, [])
+
     return (
         <React.Fragment>
             <hr />
+
+            <h2 ref={ref => h2ref = ref}  className="clients-title medium-title text-center">Our Clients</h2>
             
-            <h2 className="medium-title text-center">Our Clients</h2>
-            <div className="w-100 d-flex flex-row client-imgs-container">
+            <div ref={ref => divRef = ref} className="d-flex flex-row client-imgs-container">
                 <img className="client-img" src={require('../../images/sky.png')} alt="sky" />
                 <img className="client-img" src={require('../../images/reuters.png')} alt="thomson_reuters" />
                 <img className="client-img" src={require('../../images/infabode.png')} alt="infabode" />
