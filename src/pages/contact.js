52a4043d0fea0a3d6a90f5b914462bef2a76cda6
Aid Thompson <thePandaRiot@gmail.com>
2020-07-08 21:25:08 +0100

    anims for fade in laptop man on ideas row

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,17 +14,38 @@ const ContactPage = () => {
 
       <SEO title="Contact" />
 
-      <div className="row full-page-height">
+      <div className="row d-flex justify-content-center full-page-height">
 
 
-        <div className="col-md-7 d-flex justify-content-center text-center">
-          <h1 className="white d-flex align-items-center">Say Hi!</h1>
+        <div className="col-md-12 d-flex justify-content-center text-center">
+          <h1 className="white d-flex align-items-center text-align-center">Say Hi!</h1>
+        </div>
+
+
+
+
+        <div className="col-md-5">
+
+          <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Message: <textarea placeholder="How can we help?" name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </div>
 
 
       </div>
 
-      
+
 
     </Layout>
   )
