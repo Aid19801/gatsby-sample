f5790fd2923253c49a44329fcd93a8d8832cbb88
Aid Thompson <thePandaRiot@gmail.com>
2020-07-09 16:53:08 +0100

    anims for fade in laptop man on ideas row

--- src/components/ClientList/index.js
+++ src/components/ClientList/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { gsap, Power3, TimelineLite } from "gsap"
+import React, { useEffect, useRef } from 'react';
+import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 import '../../styles/ClientList.css';
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import favicon from '../images/f27_small.png'
+import Helmet from 'react-helmet'
 import Header from "./header"
 import "./layout.css"
 import "../styles/utils.css";
@@ -20,19 +22,25 @@ const Layout = ({ children }) => {
 
   return (
     <React.Fragment>
+
+
+      <Helmet>
+        <link rel="icon" href={favicon} />
+      </Helmet>
+
       <Header siteTitle={data.site.siteMetadata.initials} />
       <div
         style={{
           margin: `0 auto`,
           background: '#68a39e',
-          
+
         }}
         className="container-fluid nopadding"
       >
 
         <main>{children}</main>
         <footer className="cursive white text-align-center">
-          © {new Date().getFullYear()}, 
+          © {new Date().getFullYear()},
           {` `}
           <a className="cursive white text-align-center" href="https://www.linkedin.com/in/adrian-thompson-961b7114a/">Aid Thompson</a>
         </footer>
